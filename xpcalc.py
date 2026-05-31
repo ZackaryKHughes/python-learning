800570fc34874d30d38188e34d0772605d25059d
Zackary Hughes <zackary.k.hughes@gmail.com>
2026-05-31 16:51:18 -0700

    Updated a calculator to take logic given from user and give them feedback to how much xp they need left to reach desired goal. Once reached let them know they have reached the goal.

--- xpcalc.py
+++ xpcalc.py
@@ -1,6 +1,14 @@
 current_xp = int(input("Enter your current XP: "))
-goal_xp =int(input("Enter your goal XP: "))
+goal_xp = int(input("Enter your goal XP: "))
+xp_needed = goal_xp - current_xp
 
-xP_needed = goal_xp - current_xp
 
-print("You need", xP_needed, "XP to reach your goal.")
+if xp_needed <= 0:
+    print("Congratulations! You have already reached your goal XP!")
+else:
+    while xp_needed > 0:
+        print("You need", xp_needed, "XP to reach your goal.")
+        xp_needed -= 1000
+        print("Keep training! You are making progress.")
+
+    print("Congratulations! You have reached your goal XP!")
