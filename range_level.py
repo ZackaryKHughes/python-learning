f8ad11021f11ccabd8ddf9ed446c865f9378dbc9
Zackary Hughes <zackaryhughes@Zackarys-MacBook-Air.local>
2026-05-31 14:11:13 -0700

    Added ranged weapon recommendation program.

---
+++ range_level.py
@@ -0,0 +1,12 @@
+range_level = int(input("What is your range level?"))
+
+if range_level >= 90:
+    print("You can use a Dragon Hunter Crossbow. You are crushing it!")
+elif range_level >= 75:
+    print("You can use a Toxic Blowpipe. You are doing great!")
+elif range_level >= 61:
+    print("You can use a Rune Crossbow. You are okay I guess.")
+else:
+    print("You need to step it up if you want to be a good ranger. You can only use a Maple Shortbow.")
+
+print("Range is my favorite skill to train.")
