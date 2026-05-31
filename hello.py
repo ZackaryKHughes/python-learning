a1fda51fe839e800bc28dd04efc586ac62eb3845
Zackary Hughes <zackaryhughes@Zackarys-MacBook-Air.local>
2026-05-31 12:54:33 -0700

    Initial Python Projects

---
+++ hello.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+job = input("What is your job? ")
+shoe_size = input("What is your shoe size? ")
+dream_job = input("What is your dream job? ")
+onewish = input("What is one wish you have? ")  
+
+
+print("My name is:", name, "and I am", age, "years old. I work as a", job, "and my shoe size is", shoe_size,". My dream job is to be a", dream_job, "and one wish I have is", onewish,".")
---
+++ xpcalc.py
@@ -0,0 +1,6 @@
+current_xp = int(input("Enter your current XP: "))
+goal_xp =int(input("Enter your goal XP: "))
+
+xP_needed = goal_xp - current_xp
+
+print("You need", xP_needed, "XP to reach your goal.")
