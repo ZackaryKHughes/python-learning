8884e9cb6e1429e28feab2290c678ab9f308d745
Zackary Hughes <zackary.k.hughes@gmail.com>
2026-05-31 19:08:15 -0700

    Indentified some logic mistake where if someone already had the necessary xp it would now hit the break and immediatly go to the print Congrats....

--- xpcalc.py
+++ xpcalc.py
@@ -9,7 +9,9 @@ if xp_needed <= 0:
 else:
     while xp_needed > 0:
         print("You need", xp_needed, "XP to reach your goal.")
-        xp_needed -= int(xp_per_kill)
+        xp_needed -= xp_per_kill
+        if xp_needed <=0:
+            break
         print("Keep training! You need " + str(xp_needed / xp_per_kill) + " kills to reach your goal.")
 
     print("Congratulations! You have reached your goal XP!")
