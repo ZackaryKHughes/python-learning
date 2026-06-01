98f45151485b1ba5e3101c98bd2b0243a5d3e2e6
Zackary Hughes <zackary.k.hughes@gmail.com>
2026-05-31 18:59:45 -0700

    Changed text.

--- xpcalc.py
+++ xpcalc.py
@@ -1,7 +1,7 @@
 current_xp = int(input("Enter your current XP: "))
 goal_xp = int(input("Enter your goal XP: "))
 xp_needed = goal_xp - current_xp
-xp_per_kill = int(input("Enter the XP you get per Black Demon kill: "))
+xp_per_kill = int(input("Enter the XP you get per kill: "))
 
 
 if xp_needed <= 0:
