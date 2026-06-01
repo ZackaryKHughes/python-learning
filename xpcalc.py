d610f056adbe1a981bb31b0c01ab4c7190d5cd4b
Zackary Hughes <zackary.k.hughes@gmail.com>
2026-05-31 17:04:13 -0700

    Trying to add math involved to know exactly how many kills you need to reach that goal.

--- xpcalc.py
+++ xpcalc.py
@@ -1,6 +1,7 @@
 current_xp = int(input("Enter your current XP: "))
 goal_xp = int(input("Enter your goal XP: "))
 xp_needed = goal_xp - current_xp
+per_black_demon = int(input("Enter the XP you get per Black Demon kill: "))
 
 
 if xp_needed <= 0:
@@ -8,7 +9,7 @@ if xp_needed <= 0:
 else:
     while xp_needed > 0:
         print("You need", xp_needed, "XP to reach your goal.")
-        xp_needed -= 1000
-        print("Keep training! You are making progress.")
+        xp_needed -= int(per_black_demon)
+        print("Keep training! You need " + str(xp_needed / per_black_demon) + " kills to reach your goal.")
 
     print("Congratulations! You have reached your goal XP!")
