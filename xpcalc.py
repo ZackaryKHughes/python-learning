f22fb9874c4f5037d99e4921d1affb23cac6c8bb
Zackary Hughes <zackary.k.hughes@gmail.com>
2026-05-31 18:58:37 -0700

    change the name for xp to a more generic kills instead of 1 certain one.

--- xpcalc.py
+++ xpcalc.py
@@ -1,7 +1,7 @@
 current_xp = int(input("Enter your current XP: "))
 goal_xp = int(input("Enter your goal XP: "))
 xp_needed = goal_xp - current_xp
-per_black_demon = int(input("Enter the XP you get per Black Demon kill: "))
+xp_per_kill = int(input("Enter the XP you get per Black Demon kill: "))
 
 
 if xp_needed <= 0:
@@ -9,7 +9,7 @@ if xp_needed <= 0:
 else:
     while xp_needed > 0:
         print("You need", xp_needed, "XP to reach your goal.")
-        xp_needed -= int(per_black_demon)
-        print("Keep training! You need " + str(xp_needed / per_black_demon) + " kills to reach your goal.")
+        xp_needed -= int(xp_per_kill)
+        print("Keep training! You need " + str(xp_needed / xp_per_kill) + " kills to reach your goal.")
 
     print("Congratulations! You have reached your goal XP!")
