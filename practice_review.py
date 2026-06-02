07382ff41da3bb481848d77642ace659cafb8188
Zackary Hughes <zackary.k.hughes@gmail.com>
2026-06-02 10:24:16 -0700

    Practicing from start to finish what I have learned so far in one place with notes.

---
+++ practice_review.py
@@ -0,0 +1,29 @@
+#============================
+# Python Practice Review
+#============================
+
+print("Welcome to Python practice review!")
+print("----------------------------------")
+
+# Variables and Data Types
+
+name= "Zackary"
+age = 31
+weight = 220.2
+is_learning = True
+
+print(name)
+print(age)
+print(weight)
+print(is_learning)
+
+# What type is name? String (Text)
+print(type(name))
+# What type is age? Integer (whole number)
+print(type(age))
+# What type is weight? Float (decimal number))
+print(type(weight))
+# What type is is_learning? Boolean (True or False)
+print(type(is_learning))
+# What will print to the screen? "Zackary is 31 years old, weighs 220.2 pounds, and is learning Python: True"
+print(f"{name} is {age} years old, weighs {weight} pounds, and is learning Python: {is_learning}")
